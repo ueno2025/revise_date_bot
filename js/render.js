a7c6060db123f70f5aca81c8e644c5328db0a422
ueno2025 <ken825325@outlook.jp>
2025-10-17 16:01:12 +0900

    Update render.js

--- js/render.js
+++ js/render.js
@@ -1,6 +1,6 @@
 export async function render_data(processed_data) {
     const allRanges = [];
-
+    var revise_num = 0;
 
     // 各企業の修正時期を平坦化
     processed_data.forEach(company => {
@@ -10,6 +10,7 @@ export async function render_data(processed_data) {
         const filteredRanges = company.修正時期.filter(range => range.length >= 3);
 
         filteredRanges.forEach(range => {
+            revise_num += 1;
             allRanges.push({
                 companyCode: company.証券コード,
                 companyName: company.企業名,
@@ -24,8 +25,8 @@ export async function render_data(processed_data) {
     
     // n日前の日付を基準日に設定
     let n = 0
-    // const h1 = document.getElementById("h1");
-    // h1.innerHTML += `→${n}日前のデータから取得`;
+    const sub_text = document.getElementsByClassName("sub-text");
+    sub_text[0].innerHTML += ` 合計:${revise_num}件`
     const today = new Date();
     const baseDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() - n);
     // 日付に近い順で昇順ソート（lastDateを基準）
